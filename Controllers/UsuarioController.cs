8c5b64eacf69cb51dd0562f5a7c555bdd2065395
thiagodacosta85 <thiago.costa85@outlook.com>
2021-01-09 16:43:41 -0300

    Primeiras implementacoes

---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,25 @@
+﻿using BTC_Avnd_ConfArqBackEnd.Models.Usuarios;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BTC_Avnd_ConfArqBackEnd.Controllers
+{
+    [Route("api/v1/usuario")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        
+        [HttpPost]
+        [Route("Logar")]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+            return Ok(loginViewModelInput);
+        }
+
+        [HttpPost]
+        [Route("registrar")]
+        public IActionResult Registrar(RegistroViewModelInput loginViewModelInput)
+        {
+            return Created("", loginViewModelInput);
+        }
+    }
+}
---
+++ Models/Usuarios/RegistroViewModelInput.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BTC_Avnd_ConfArqBackEnd.Models.Usuarios
+{
+    public class RegistroViewModelInput
+    {
+        [Required(ErrorMessage = "O login é obrigatório.")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "O E-mail é obrigatório.")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "A senha é obrigatória.")]
+        public string Senha { get; set; }
+    }
+}
